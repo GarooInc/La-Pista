24745a4e9bca37f64eee4695c50715a18d7583e9
Jimena Hernández <her21199@uvg.edu.gt>
2023-09-06 12:54:05 -0600

    actualizacion fotos

--- src/galery.js
+++ src/galery.js
@@ -1,68 +1,28 @@
 export const galeryImages = [
     {
         id: 1,
-        src: "/images/galery/1.jpeg",
+        src: "/images/galery/1.jpg",
         alt: "1"
     },
     {
         id: 2,
-        src: "/images/galery/2.jpeg",
+        src: "/images/galery/2.jpg",
         alt: "2"
     },
     {
         id: 3,
-        src: "/images/galery/3.jpeg",
+        src: "/images/galery/3.jpg",
         alt: "3"
     },
     {
         id: 4,
-        src: "/images/galery/4.jpeg",
+        src: "/images/galery/4.jpg",
         alt: "4"
     },
     {
         id: 5,
-        src: "/images/galery/5.jpeg",
+        src: "/images/galery/5.jpg",
         alt: "5"
-    },
-    {
-        id: 6,
-        src: "/images/galery/6.jpeg",
-        alt: "6"
-    },
-    {
-        id: 7,
-        src: "/images/galery/7.jpg",
-        alt: "7"
-    },
-    {
-        id: 9,
-        src: "/images/galery/9.jpeg",
-        alt: "9"
-    },
-    {
-        id: 10,
-        src: "/images/galery/10.jpeg",
-        alt: "10"
-    },
-    {
-        id: 11,
-        src: "/images/galery/11.jpeg",
-        alt: "11"
-    },
-    {
-        id: 12,
-        src: "/images/galery/12.jpeg",
-        alt: "12"
-    },
-    {
-        id: 13,
-        src: "/images/galery/13.jpeg",
-        alt: "13"
-    },
-    {
-        id: 14,
-        src: "/images/galery/14.jpg",
-        alt: "14"
     }
 ]
 
